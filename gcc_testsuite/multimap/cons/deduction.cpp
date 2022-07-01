8654fe007e090424874369f3dccb8751616becaf
Georgii Buzykin <gbuzykin@yandex.ru>
2022-07-01 07:46:16 +0300

    GCC testsuite: Add `uxs::multimap<>` tests

---
+++ gcc_testsuite/multimap/cons/deduction.cpp
@@ -0,0 +1,118 @@
+// { dg-do compile { target c++17 } }
+
+#if __cplusplus >= 201703L
+
+#    include "gcc_testsuite/util/testsuite_allocator.h"
+#    include "gcc_testsuite/util/testsuite_iterators.h"
+
+#    include "uxs/multimap.h"
+
+using __gnu_test::SimpleAllocator;
+using value_type = uxs::multimap<int, double>::value_type;
+
+static_assert(
+    std::is_same_v<decltype(uxs::multimap{value_type{1, 2.0}, {2, 3.0}, {3, 4.0}}), uxs::multimap<int, double>>);
+
+static_assert(std::is_same_v<decltype(uxs::multimap{std::pair{1, 2.0}, {2, 3.0}, {3, 4.0}}), uxs::multimap<int, double>>);
+
+static_assert(
+    std::is_same_v<decltype(uxs::multimap{{value_type{1, 2.0}, {2, 3.0}, {3, 4.0}}}), uxs::multimap<int, double>>);
+
+static_assert(
+    std::is_same_v<decltype(uxs::multimap{{std::pair{1, 2.0}, {2, 3.0}, {3, 4.0}}}), uxs::multimap<int, double>>);
+
+static_assert(std::is_same_v<decltype(uxs::multimap{{value_type{1, 2.0}, {2, 3.0}, {3, 4.0}}, std::less<int>{}, {}}),
+                             uxs::multimap<int, double>>);
+
+static_assert(std::is_same_v<decltype(uxs::multimap{{std::pair{1, 2.0}, {2, 3.0}, {3, 4.0}}, std::less<int>{}, {}}),
+                             uxs::multimap<int, double>>);
+
+/* This is not deducible, {} could be deduced as _Compare or _Allocator.
+static_assert(std::is_same_v<
+          decltype(uxs::multimap{{value_type{1, 2.0}, {2, 3.0}, {3, 4.0}},
+                     {}}),
+          uxs::multimap<int, double>>);
+*/
+
+static_assert(std::is_same_v<decltype(uxs::multimap{{std::pair{1, 2.0}, {2, 3.0}, {3, 4.0}}, std::less<int>{}}),
+                             uxs::multimap<int, double>>);
+
+static_assert(
+    std::is_same_v<decltype(uxs::multimap{{value_type{1, 2.0}, {2, 3.0}, {3, 4.0}}, {}, SimpleAllocator<value_type>{}}),
+                   uxs::multimap<int, double, std::less<int>, SimpleAllocator<value_type>>>);
+
+static_assert(
+    std::is_same_v<decltype(uxs::multimap{{std::pair{1, 2.0}, {2, 3.0}, {3, 4.0}}, {}, SimpleAllocator<value_type>{}}),
+                   uxs::multimap<int, double, std::less<int>, SimpleAllocator<value_type>>>);
+
+void f() {
+    uxs::multimap<int, double> x;
+    static_assert(std::is_same_v<decltype(uxs::multimap(x.begin(), x.end())), uxs::multimap<int, double>>);
+
+    static_assert(
+        std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), std::less<int>{}, std::allocator<value_type>{}}),
+                       uxs::multimap<int, double>>);
+
+    static_assert(
+        std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), std::less<int>{}, {}}), uxs::multimap<int, double>>);
+
+    static_assert(
+        std::is_same_v<decltype(uxs::multimap(x.begin(), x.end(), std::less<int>{})), uxs::multimap<int, double>>);
+
+    static_assert(std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), {}, std::allocator<value_type>{}}),
+                                 uxs::multimap<int, double>>);
+
+    static_assert(std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), {}, SimpleAllocator<value_type>{}}),
+                                 uxs::multimap<int, double, std::less<int>, SimpleAllocator<value_type>>>);
+}
+
+using __gnu_test::input_iterator_wrapper;
+using __gnu_test::test_container;
+
+void g() {
+    value_type array[1];
+    test_container<value_type, input_iterator_wrapper> x(array);
+
+    static_assert(std::is_same_v<decltype(uxs::multimap(x.begin(), x.end())), uxs::multimap<int, double>>);
+
+    static_assert(
+        std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), std::less<int>{}, std::allocator<value_type>{}}),
+                       uxs::multimap<int, double>>);
+
+    static_assert(
+        std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), std::less<int>{}, {}}), uxs::multimap<int, double>>);
+
+    static_assert(
+        std::is_same_v<decltype(uxs::multimap(x.begin(), x.end(), std::less<int>{})), uxs::multimap<int, double>>);
+
+    static_assert(std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), {}, std::allocator<value_type>{}}),
+                                 uxs::multimap<int, double>>);
+
+    static_assert(std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), {}, SimpleAllocator<value_type>{}}),
+                                 uxs::multimap<int, double, std::less<int>, SimpleAllocator<value_type>>>);
+}
+
+void h() {
+    std::pair<int, double> array[1];
+    test_container<std::pair<int, double>, input_iterator_wrapper> x(array);
+
+    static_assert(std::is_same_v<decltype(uxs::multimap(x.begin(), x.end())), uxs::multimap<int, double>>);
+
+    static_assert(
+        std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), std::less<int>{}, std::allocator<value_type>{}}),
+                       uxs::multimap<int, double>>);
+
+    static_assert(
+        std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), std::less<int>{}, {}}), uxs::multimap<int, double>>);
+
+    static_assert(
+        std::is_same_v<decltype(uxs::multimap(x.begin(), x.end(), std::less<int>{})), uxs::multimap<int, double>>);
+
+    static_assert(std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), {}, std::allocator<value_type>{}}),
+                                 uxs::multimap<int, double>>);
+
+    static_assert(std::is_same_v<decltype(uxs::multimap{x.begin(), x.end(), {}, SimpleAllocator<value_type>{}}),
+                                 uxs::multimap<int, double, std::less<int>, SimpleAllocator<value_type>>>);
+}
+
+#endif
---
+++ gcc_testsuite/multimap/modifiers/swap/1.cpp
@@ -0,0 +1,75 @@
+// Copyright (C) 2004-2022 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include "gcc_testsuite/util/testsuite_hooks.h"
+
+#include "uxs/multimap.h"
+
+namespace {
+
+#if __cplusplus >= 201103L
+#    define NOTHROW noexcept
+#else
+#    define NOTHROW
+#endif
+
+struct T {
+    int i;
+};
+
+// T must be LessThanComparable to pass concept-checks
+bool operator<(T l, T r) { return l.i < r.i; }
+
+int swap_calls;
+
+}  // namespace
+
+namespace uxs {
+template<>
+void multimap<T, int>::swap(multimap<T, int>&) NOTHROW {
+    ++swap_calls;
+}
+}  // namespace uxs
+
+namespace {
+
+// Should use multimap specialization for swap.
+int test01() {
+    uxs::multimap<T, int> A;
+    uxs::multimap<T, int> B;
+    swap_calls = 0;
+    std::swap(A, B);
+    VERIFY(1 == swap_calls);
+    return 0;
+}
+
+// Should use multimap specialization for swap.
+int test02() {
+    using namespace std;
+    uxs::multimap<T, int> A;
+    uxs::multimap<T, int> B;
+    swap_calls = 0;
+    swap(A, B);
+    VERIFY(1 == swap_calls);
+    return 0;
+}
+
+}  // namespace
+
+// See c++/13658 for background info.
+ADD_TEST_CASE("", "multimap", test01);
+ADD_TEST_CASE("", "multimap", test02);
