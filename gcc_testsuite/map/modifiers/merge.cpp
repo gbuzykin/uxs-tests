ccf700bbf5aa4916cf5509339a05b886bbd92335
Georgii Buzykin <gbuzykin@yandex.ru>
2022-07-01 07:46:13 +0300

    GCC testsuite: Add `uxs::map<>` tests

---
+++ gcc_testsuite/map/modifiers/merge.cpp
@@ -0,0 +1,143 @@
+// Copyright (C) 2016-2022 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+// { dg-do run { target c++17 } }
+
+#include "gcc_testsuite/util/testsuite_hooks.h"
+
+#include "uxs/functional.h"
+#include "uxs/map.h"
+#include "uxs/multimap.h"
+
+#include <algorithm>
+
+namespace {
+
+using test_type = uxs::map<int, int>;
+
+int test01() {
+    const test_type c0{{1, 10}, {2, 20}, {3, 30}};
+    test_type c1 = c0, c2 = c0;
+
+    c1.merge(c2);
+    VERIFY(c1 == c0);
+    VERIFY(c2 == c0);
+
+    c1.clear();
+    c1.merge(c2);
+    VERIFY(c1 == c0);
+    VERIFY(c2.empty());
+
+    c2.merge(std::move(c1));
+    VERIFY(c1.empty());
+    VERIFY(c2 == c0);
+
+    return 0;
+}
+
+int test02() {
+    const test_type c0{{1, 10}, {2, 20}, {3, 30}};
+    test_type c1 = c0;
+    uxs::map<int, int, uxs::less<>> c2(c0.begin(), c0.end());
+
+    c1.merge(c2);
+    VERIFY(c1 == c0);
+    VERIFY(c2.size() == c0.size() && std::equal(c2.begin(), c2.end(), c0.begin()));
+
+    c1.clear();
+    c1.merge(c2);
+    VERIFY(c1 == c0);
+    VERIFY(c2.empty());
+
+    c2.merge(std::move(c1));
+    VERIFY(c1.empty());
+    VERIFY(c2.size() == c0.size() && std::equal(c2.begin(), c2.end(), c0.begin()));
+
+    return 0;
+}
+
+int test03() {
+    const test_type c0{{1, 10}, {2, 20}, {3, 30}};
+    test_type c1 = c0;
+    uxs::map<int, int, uxs::greater<>> c2(c0.begin(), c0.end());
+
+    c1.merge(c2);
+    VERIFY(c1 == c0);
+    VERIFY(c2.size() == c0.size() && std::equal(c2.rbegin(), c2.rend(), c0.begin()));
+
+    c1.clear();
+    c1.merge(c2);
+    VERIFY(c1 == c0);
+    VERIFY(c2.empty());
+
+    c2.merge(c1);
+    VERIFY(c1.empty());
+    VERIFY(c2.size() == c0.size() && std::equal(c2.rbegin(), c2.rend(), c0.begin()));
+
+    c1.merge(std::move(c2));
+    VERIFY(c1 == c0);
+    VERIFY(c2.empty());
+
+    return 0;
+}
+
+int test04() {
+    const test_type c0{{1, 10}, {2, 20}, {3, 30}};
+    test_type c1 = c0;
+    uxs::multimap<int, int, uxs::greater<>> c2(c0.begin(), c0.end());
+
+    c1.merge(c2);
+    VERIFY(c1 == c0);
+    VERIFY(c2.size() == c0.size() && std::equal(c2.rbegin(), c2.rend(), c0.begin()));
+
+    c1.clear();
+    c1.merge(c2);
+    VERIFY(c1 == c0);
+    VERIFY(c2.empty());
+
+    c2.merge(c1);
+    VERIFY(c1.empty());
+    VERIFY(c2.size() == c0.size() && std::equal(c2.rbegin(), c2.rend(), c0.begin()));
+
+    c1 = c0;
+    c2.merge(c1);
+    VERIFY(c1.empty());
+    VERIFY(c2.size() == (2 * c0.size()));
+    VERIFY(std::is_sorted(c2.begin(), c2.end(), c2.value_comp()));
+
+    c1.merge(c2);
+    VERIFY(c1 == c0);
+    VERIFY(c2.size() == c0.size() && std::equal(c2.rbegin(), c2.rend(), c0.begin()));
+
+    c1.merge(std::move(c2));
+    VERIFY(c1 == c0);
+    VERIFY(c2.size() == c0.size() && std::equal(c2.rbegin(), c2.rend(), c0.begin()));
+
+    c1.clear();
+    c1.merge(std::move(c2));
+    VERIFY(c1 == c0);
+    VERIFY(c2.empty());
+
+    return 0;
+}
+
+}  // namespace
+
+ADD_TEST_CASE("", "map", test01);
+ADD_TEST_CASE("", "map", test02);
+ADD_TEST_CASE("", "map", test03);
+ADD_TEST_CASE("", "map", test04);
---
+++ gcc_testsuite/map/modifiers/swap/1.cpp
@@ -0,0 +1,74 @@
+// Copyright (C) 2004-2022 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
+
+#include "gcc_testsuite/util/testsuite_hooks.h"
+
+#include "uxs/map.h"
+
+#if __cplusplus >= 201103L
+#    define NOTHROW noexcept
+#else
+#    define NOTHROW
+#endif
+
+namespace {
+
+struct T {
+    int i;
+};
+
+// T must be LessThanComparable to pass concept-checks
+bool operator<(T l, T r) { return l.i < r.i; }
+
+int swap_calls;
+
+}  // namespace
+
+namespace uxs {
+template<>
+void map<T, int>::swap(map<T, int>&) NOTHROW {
+    ++swap_calls;
+}
+}  // namespace uxs
+
+namespace {
+
+// Should use map specialization for swap.
+int test01() {
+    uxs::map<T, int> A;
+    uxs::map<T, int> B;
+    swap_calls = 0;
+    std::swap(A, B);
+    VERIFY(1 == swap_calls);
+    return 0;
+}
+
+// Should use map specialization for swap.
+int test02() {
+    using namespace std;
+    uxs::map<T, int> A;
+    uxs::map<T, int> B;
+    swap_calls = 0;
+    swap(A, B);
+    VERIFY(1 == swap_calls);
+    return 0;
+}
+
+}  // namespace
+
+ADD_TEST_CASE("", "map", test01);
+ADD_TEST_CASE("", "map", test02);
