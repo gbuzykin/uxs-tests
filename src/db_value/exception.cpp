0ff1d13d7ca5c4ba7ba9959bdbcde31e4ce534c4
Georgii Buzykin <gbuzykin@yandex.ru>
2022-07-11 16:40:24 +0300

    Customize `db::value` character type and allocators

--- src/db_value/exception.cpp
+++ src/db_value/exception.cpp
@@ -12,79 +12,79 @@ int test_exception_safety() {
     try {
         uxs::db::value v(bad_str);
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         uxs::db::value v;
         v = bad_str;
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     // initialize with array with bad string (from random access range)
     try {
         std::vector<std::string_view> init{"1", "2", "3", bad_str};
         uxs::db::value v(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::vector<std::string_view> init{"1", "2", "3", bad_str};
         uxs::db::value v = uxs::db::make_array(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     // initialize with array with bad string (from generic access range)
     try {
         std::list<std::string_view> init{"1", "2", "3", bad_str};
         uxs::db::value v(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::list<std::string_view> init{"1", "2", "3", bad_str};
         uxs::db::value v = uxs::db::make_array(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     // assignment of array with bad string
     try {
         std::vector<std::string_view> init{"1", "2", "3", bad_str};
         uxs::db::value v{"1", "2", "3"};
         v.assign(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::vector<std::string_view> init{"1", bad_str};
         uxs::db::value v{"1", "2", "3"};
         v.assign(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     // initialize with record with bad string
     try {
         std::vector<std::pair<std::string_view, std::string_view>> init{
             {"1", "A"}, {"2", "B"}, {"3", "C"}, {bad_str, "D"}};
         uxs::db::value v(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::vector<std::pair<std::string_view, std::string_view>> init{
             {"1", "A"}, {"2", "B"}, {"3", "C"}, {bad_str, "D"}};
         uxs::db::value v = uxs::db::make_record(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::vector<std::pair<std::string_view, std::string_view>> init{
             {"1", "A"}, {"2", "B"}, {"3", "C"}, {"4", bad_str}};
         uxs::db::value v(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::vector<std::pair<std::string_view, std::string_view>> init{
             {"1", "A"}, {"2", "B"}, {"3", "C"}, {"4", bad_str}};
         uxs::db::value v = uxs::db::make_record(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     // initialize with record initializer with bad string
     try {
         std::initializer_list<std::pair<std::string_view, uxs::db::value>> init{
             {"1", "A"}, {"2", "B"}, {"3", "C"}, {bad_str, "D"}};
         uxs::db::value v = uxs::db::make_record(init);
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     // assignment of record with bad string
     try {
         std::vector<std::pair<std::string_view, std::string_view>> init{
@@ -92,26 +92,26 @@ int test_exception_safety() {
         uxs::db::value v{{"1", "A"}, {"2", "B"}, {"3", "C"}};
         v.assign(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::vector<std::pair<std::string_view, std::string_view>> init{{"1", "A"}, {bad_str, "D"}};
         uxs::db::value v{{"1", "A"}, {"2", "B"}, {"3", "C"}};
         v.assign(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::vector<std::pair<std::string_view, std::string_view>> init{
             {"1", "A"}, {"2", "B"}, {"3", "C"}, {"4", bad_str}};
         uxs::db::value v{{"1", "A"}, {"2", "B"}, {"3", "C"}};
         v.assign(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::vector<std::pair<std::string_view, std::string_view>> init{{"1", "A"}, {"4", bad_str}};
         uxs::db::value v{{"1", "A"}, {"2", "B"}, {"3", "C"}};
         v.assign(init.begin(), init.end());
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     // insert of record initializer with bad string
     try {
         std::initializer_list<std::pair<std::string_view, uxs::db::value>> init{
@@ -119,14 +119,14 @@ int test_exception_safety() {
         uxs::db::value v;
         v.insert(init);
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     try {
         std::initializer_list<std::pair<std::string_view, uxs::db::value>> init{
             {"1", "A"}, {"2", "B"}, {"3", "C"}, {bad_str, "D"}};
         uxs::db::value v{{"1", "A"}, {"2", "B"}, {"3", "C"}};
         v.insert(init);
         VERIFY(false);
-    } catch (const std::exception& e) {}
+    } catch (const std::exception&) {}
     return 0;
 }
 
