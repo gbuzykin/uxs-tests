e64457953155a66a64210881e4f205cf556e34cf
Georgii Buzykin <gbuzykin@yandex.ru>
2023-09-21 17:58:04 +0300

    Move `uxs` to separate shared library

--- src/db_value/custom_alloc.cpp
+++ src/db_value/custom_alloc.cpp
@@ -1,3 +1,5 @@
+#ifndef _MSC_VER
+
 #include "db_value_tests.h"
 
 #include "uxs/db/value_impl.h"
@@ -10,3 +12,5 @@ template class basic_value<char, test_allocator<char>>;
 template class basic_value<wchar_t, test_allocator<wchar_t>>;
 }  // namespace db
 }  // namespace uxs
+
+#endif
