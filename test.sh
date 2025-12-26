abdbf18740a9f1909da7428ef5a2626a6d9114c5
Georgii Buzykin <gbuzykin@yandex.ru>
2025-12-26 12:08:41 +0300

    Update `uxs` library

--- test.sh
+++ test.sh
@@ -5,12 +5,13 @@ CXX_STD=${CXX_STD:-20}
 USE_LIBCPP=${USE_LIBCPP:-OFF}
 BUILD_TYPE=${BUILD_TYPE:-Debug}
 N_PROC=${N_PROC:-8}
-BUILD_DIR=./build-test-$CXX_COMPILER-C++$CXX_STD
-if [[ $CXX_COMPILER == clang* ]] && [[ $USE_LIBCPP == ON ]]; then BUILD_DIR=$BUILD_DIR-libc++; fi
-BUILD_DIR=$BUILD_DIR-$BUILD_TYPE
+BUILD_OPTS=$CXX_COMPILER-C++$CXX_STD
+if [[ $CXX_COMPILER == clang* ]] && [[ $USE_LIBCPP == ON ]]; then BUILD_OPTS=$BUILD_OPTS-libc++; fi
+BUILD_OPTS=$BUILD_OPTS-$BUILD_TYPE
+BUILD_DIR=./build-test-$BUILD_OPTS
 echo "---- Checking C++$CXX_STD $BUILD_TYPE compilation"
-mkdir -p $BUILD_DIR
 cmake -S . -B $BUILD_DIR -G "Ninja" \
+  -DOPTION_TREAT_WARNINGS_AS_ERRORS=ON \
   -DCMAKE_C_COMPILER=$C_COMPILER \
   -DCMAKE_CXX_COMPILER=$CXX_COMPILER \
   -DCMAKE_CXX_STANDARD=$CXX_STD \
@@ -19,5 +20,7 @@ cmake -S . -B $BUILD_DIR -G "Ninja" \
 cmake --build $BUILD_DIR -j $N_PROC
 if [[ $COMPILE_ONLY != ON ]]; then
   echo "---- Runing tests for C++$CXX_STD $BUILD_TYPE compilation"
-  $BUILD_DIR/uxs-tests -d testdata $*
+  INSTALL_DIR=./install-test-$BUILD_OPTS
+  cmake --install $BUILD_DIR --prefix $INSTALL_DIR
+  $INSTALL_DIR/bin/uxs-tests -d testdata $*
 fi
